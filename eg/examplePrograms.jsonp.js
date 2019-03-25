20c5fda1ea45e6ee4f684fddaeccd8ecff669ba7
Chris Pressey <cpressey@catseye.tc>
2019-03-25 14:38:13 +0000

    Add some example programs. Load them in the web-based demo.

---
+++ eg/examplePrograms.jsonp.js
@@ -0,0 +1,26 @@
+examplePrograms = [
+    {
+        "contents": "3\n*>\n9+@\n/>\n2\n", 
+        "filename": "arithmetic.flobnar"
+    }, 
+    {
+        "contents": ">>>>>v\n^    v\n^    4\n^<<<<@\n", 
+        "filename": "arrows.flobnar"
+    }, 
+    {
+        "contents": ">     v\n^\\ <   \n       \n:v    v   \\<@\n-<      : 6\n1 :   > *\n  -|    <\n  11\n", 
+        "filename": "factorial.flobnar"
+    }, 
+    {
+        "contents": "  3\n\n9 | @\n\n  4\n", 
+        "filename": "if.flobnar"
+    }, 
+    {
+        "contents": "     5\n85   #\n*p<\n40+@\n  >  ^\n     6\n     9\n", 
+        "filename": "self-mod.flobnar"
+    }, 
+    {
+        "contents": "            \n    v   @   \n   #<  17   \n            \n", 
+        "filename": "wrapping.flobnar"
+    }
+];
